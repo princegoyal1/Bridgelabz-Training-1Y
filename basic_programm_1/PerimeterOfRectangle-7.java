9248855fe3a2842e422b9f4423a2c7f3ee0c4039
princegoyal1 <princegoyal0060.com>
2026-01-28 16:32:31 +0530

    [Prince] files added

---
+++ basic_programm_1/PerimeterOfRectangle-7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length: ");
+        int length = sc.nextInt();
+        System.out.print("Enter width: ");
+        int width = sc.nextInt();
+        int perimeter = 2 * (length + width);
+        System.out.println("Perimeter of Rectangle = " + perimeter);
+    }
+}
