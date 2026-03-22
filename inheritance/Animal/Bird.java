700b14c01b2988c4b90869fbed9b94948065585e
princegoyal1 <princegoyal0060.com>
2026-03-22 21:46:22 +0530

    [Prince]

---
+++ inheritance/Animal/Bird.java
@@ -0,0 +1,8 @@
+
+package Inheritance.Animal;
+
+public class Bird extends Animal{
+    void makeSound() {
+        System.out.println("Bird says: Chirp Chirp!");
+    }
+}
---
+++ inheritance/Animal/Cat.java
@@ -0,0 +1,8 @@
+
+package Inheritance.Animal;
+
+public class Cat extends Animal{
+    void makeSound() {
+        System.out.println("Cat says: Meow Meow!");
+    }
+}
