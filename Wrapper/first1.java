ce7b16f98c92167da954e309c5a1b87146a3f8e8
princegoyal1 <princegoyal0060.com>
2026-04-28 17:15:09 +0530

    [Prince]

---
+++ Wrapper/first1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class first1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number : - ");
+        int n = sc.nextInt();
+        Integer obj = n;
+        System.out.println("Primitive Form : - "+n);
+        System.out.println("Object Form : - "+obj);
+    }
+}
